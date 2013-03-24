da49451ebb7ec2f80974764dde050574fca4bbf6
Ivo Rafael <ivo.rafael@gmail.com>
2013-03-24 11:35:35 -0300

    added jquery mobile files

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,5 @@
 //= require "_includes/jquery-1.9.1.min"
+//= require "_includes/jquery.mobile-1.3.0.min"
 //= require "_includes/cordova-2.5.0"
 //= require "_includes/cdv-plugin-fb-connect"
 //= require "_includes/facebook_js_sdk"
